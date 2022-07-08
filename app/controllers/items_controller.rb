7b5d683c6da9e94535b1ea618234c87199470027
Zitsuyu <yuichi09202001@yahoo.co.jp>
2022-07-08 17:12:30 +0900

    ActiveHash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,26 @@
 class ItemsController < ApplicationController
+  
   def index
   end
 
-  def edit
-  end
-
   def new
+    @item = Item.new
   end
 
-  def show
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-    
+
+  
+  private
+
+  def item_params
+    params.require(:item).permit( :name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category 
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :shipping_fee
+  belongs_to :status 
   belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :price, presence: true
 end
